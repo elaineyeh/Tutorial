19d12f4e319319adc2ad4b7972ce7018a7975271
elaineyeh <10456018@ntub.edu.tw>
2020-07-15 09:02:29 +0800

    Requests-and-Responses

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
