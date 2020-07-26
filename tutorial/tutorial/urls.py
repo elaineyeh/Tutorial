26279843d791eda06f4dc29140d8f7c97f6f5699
elaineyeh <10456018@ntub.edu.tw>
2020-07-26 12:16:28 +0800

    Relationships & Hyperlinked APIs

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -13,24 +13,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from django.conf.urls import include
 
-urlpatterns = [
-    path('', views.api_root),
-    path('admin/', admin.site.urls),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>', views.UserDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view())
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
-]
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path(
+        '',
+        views.api_root
+    ),
+    path(
+        'snippets/',
+        views.SnippetList.as_view(),
+        name='snippet-list'
+    ),
+    path(
+        'snippets/<int:pk>/',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail'
+    ),
+    path(
+        'snippets/<int:pk>/highlight/',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight'
+    ),
+    path(
+        'users/',
+        views.UserList.as_view(),
+        name='user-list'
+    ),
+    path(
+        'users/<int:pk>/',
+        views.UserDetail.as_view(),
+        name='user-detail'
+    )
+])
